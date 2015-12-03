19b9c8353caefb232b9b772e6e73ff6688167a48
Jasmine Giang <jasminegiang@gmail.com>
2015-12-03 01:46:42 -0800

    Initialize xcode project

---
+++ PLTR/PLTR/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  PLTR
+//
+//  Created by Jasmine Giang on 12/3/15.
+//  Copyright © 2015 Jasmine Giang. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
